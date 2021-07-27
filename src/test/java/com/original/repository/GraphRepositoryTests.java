82454362cb1e7fc36799453465a799f4a1d8ccb5
DESKTOP-VLCHJ6Q\Henrique <henriquelamontagna@gmail.com>
2021-07-27 20:16:01 -0300

    Making the unit tests for the GraphRepository

---
+++ src/test/java/com/original/repository/GraphRepositoryTests.java
@@ -0,0 +1,59 @@
+package com.original.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.original.domain.Graph;
+import com.original.utils.TestUtils;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest
+public class GraphRepositoryTests {
+	
+	@Autowired
+	private GraphRepository graphRepository;
+	
+	@Autowired
+	private TestUtils testUtils;
+	
+	
+	@Test
+	@Order(1)
+	public void shouldSaveAGraph() {
+		Graph savedGraph = graphRepository.save(testUtils.generateAPopulatedGraph());
+//		System.out.println(savedGraph);
+		assertThat(savedGraph.getId() instanceof Long).isEqualTo(true);
+	}
+	
+	@Test
+	@Order(2)
+	public void shouldGetAGraph() {
+		Optional<Graph> recoveredGraph = graphRepository.findById(1L);
+//		System.out.println(recoveredGraph.get());
+//		System.out.println(generateACharacter());
+		assertThat(recoveredGraph.isPresent()).isEqualTo(true);
+	}
+	
+	@Test
+	@Order(3)
+	public void shouldGetAllGraphs() {
+		graphRepository.save(testUtils.generateAPopulatedGraph());
+		Iterable<Graph> allGraphsList = graphRepository.findAll();
+		Long size = StreamSupport.stream(allGraphsList.spliterator(), false).count();
+		assertThat(size).isEqualTo(2L);
+	}
+
+
+}
