53139620442bd868bf66a7f4e738b66cccb074f8
DESKTOP-VLCHJ6Q\Henrique <henriquelamontagna@gmail.com>
2021-07-27 10:04:47 -0300

    Adding a default constructor ans implementing serializable Excluding Graph parameter of JSON

--- src/main/java/com/original/domain/Route.java
+++ src/main/java/com/original/domain/Route.java
@@ -1,5 +1,7 @@
 package com.original.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,16 +10,28 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "TB_ROUTES")
-public class Route {
+public class Route implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter(onMethod = @__({@JsonIgnore}))
 	private Long id;
 	
 	@Column
@@ -31,6 +45,15 @@ public class Route {
 	
 	@ManyToOne
 	@JoinColumn(name = "graph_id", nullable = false)
+	@Getter(onMethod = @__({@JsonIgnore}))
 	private Graph graph;
+	
+	public boolean equals(Route route) {
+		if (this.source.equals(route.source) && this.target.equals(route.target) && this.distance == route.getDistance()) {
+			return true;
+		} else {			
+			return false;
+		}
+	}
 
 }
