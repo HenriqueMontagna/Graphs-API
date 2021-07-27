9938791dc4c74cb4149df267077be37ac53bddc5
DESKTOP-VLCHJ6Q\Henrique <henriquelamontagna@gmail.com>
2021-07-27 20:15:15 -0300

    Creating a GraphService

--- src/main/java/com/original/service/GraphService.java
+++ src/main/java/com/original/service/GraphService.java
@@ -1,14 +1,44 @@
 package com.original.service;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.original.domain.Graph;
+import com.original.domain.Route;
+import com.original.repository.GraphRepository;
 
 @Service
 public class GraphService {
+
+	@Autowired
+	private GraphRepository graphRepository;
+
+	@Autowired
+	private RouteService routeService;
+
+	public Graph saveGraph(Graph graph) {
+		Graph createdGraph = graphRepository.save(graph);
+		List<Route> listOfRoutes = graph.getData();
+		listOfRoutes.forEach(route -> {
+			route.setGraph(createdGraph);
+			routeService.saveRoute(route);
+		});
+		return createdGraph;
+	}
 	
-	public void saveGraphInBD(Graph graph) {
+	public Graph getGraph(Long id) {
+		Graph foundGraph;
+		try {
+			foundGraph = graphRepository.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			return null;
+		} 		
 		
+		return foundGraph;
 	}
 
 }
