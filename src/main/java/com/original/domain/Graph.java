8f5ab1854f1761adfd07821922297b537b9399f9
DESKTOP-VLCHJ6Q\Henrique <henriquelamontagna@gmail.com>
2021-07-27 10:03:16 -0300

    Adding a default constructor ans implementing serializable

--- src/main/java/com/original/domain/Graph.java
+++ src/main/java/com/original/domain/Graph.java
@@ -1,5 +1,6 @@
 package com.original.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,14 +10,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "TB_GRAPHS")
-public class Graph {
+public class Graph implements Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
